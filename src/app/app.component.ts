bc8beba5ef2e7dcdec7b256cace64f7fc397df1e
nassim oueghlani <n.oueghlani@gmail.com>
2021-02-25 17:19:01 +0100

    version2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'OM', url: '/folder/OM', icon: '/assets/om.png' },
+    { title: 'PSG', url: '/folder/PSG', icon: '/assets/psg.png' },
+    { title: 'Real Madrid', url: '/folder/Real', icon: '/assets/real.png' },
+    { title: 'OL', url: '/folder/OL', icon: '/assets/ol.png' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -8,11 +8,45 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  public team;
+  public JOM = [
+    { nom: 'Dimitri Payet', num: 10, url: 'assets/payet.png', bio: 'Milieu de terrain' },
+    { nom: 'Steve Mandanda', num: 1, url: 'assets/mandanda.png', bio: 'Gardien de but' },
+    { nom: 'Florian Thauvin', num: 26, url: 'assets/thauvin.png', bio: 'Attaquant' },
+  ];
+  public JPSG = [
+    { nom: 'Neymar', num: 10, url: 'assets/neymar.png', bio: 'Attaquant' },
+    { nom: 'Killian Mbappe', num: 7, url: 'assets/mbappe.png', bio: 'Attaquant' },
+    { nom: 'Marquinhos', num: 5, url: 'assets/marquinhos.png', bio: 'Defenseur' },
+  ];
+  public JOL = [
+    { nom: 'Anthony Lopes', num: 1, url: 'assets/lopes.png', bio: 'Gardien de but' },
+    { nom: 'Houssem Aouar', num: 8, url: 'assets/aouar.png', bio: 'Milieu de terrain' },
+    { nom: 'Memphis Depay', num: 11, url: 'assets/depay.png', bio: 'Attaquant' },
+  ];
+  public JReal = [
+    { nom: 'Karim Benzema', num: 9, url: 'assets/benzema.png', bio: 'Attaquant' },
+    { nom: 'Eden Hazard', num: 7, url: 'assets/hazard.png', bio: 'Attaquant' },
+    { nom: 'Sergio Ramos', num: 4, url: 'assets/bale.png', bio: 'Defenseur' },
+  ];
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    switch (this.folder) {
+      case 'OM':
+        this.team = this.JOM;
+        break;
+      case 'PSG':
+        this.team = this.JPSG;
+        break;
+      case 'Real':
+        this.team = this.JReal;
+        break;
+      case 'OL':
+        this.team = this.JOL;
+        break;
+    }
   }
-
 }
